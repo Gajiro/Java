ad0917706ff13a26deacf1514a9bd0627679ba56
Gajiro <s.gajirou@gmail.com>
2015-09-17 15:24:09 +0900

    2015/09/17訓練

---
+++ Action/app/src/main/java/jp/co/july/tokyo/action/GameView.java
@@ -0,0 +1,84 @@
+package jp.co.july.tokyo.action;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+
+import android.view.View;
+
+/**
+ * Created by kato.yuzo on 2015/09/17.
+ */
+public class GameView extends View implements Droid.Callback {
+
+    private static final int MAX_TOUCH_TIME = 500;
+
+    private long touchDownStartTime;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                touchDownStartTime = System.currentTimeMillis();
+                return true;
+            case MotionEvent.ACTION_UP:
+                jumpDroid();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+    private void jumpDroid(){
+        float time = System.currentTimeMillis() - touchDownStartTime;
+        touchDownStartTime = 0;
+
+        if (getDistanceFromGround(droid) != 0){
+            return;
+        }
+
+        if(time > MAX_TOUCH_TIME){
+            time = MAX_TOUCH_TIME;
+
+        }
+        droid.jump(time / MAX_TOUCH_TIME);
+
+    }
+
+
+    private static final int START_GROUND_HEIGHT = 50;
+    private Ground ground;
+
+    private Droid droid;
+
+    public GameView(Context context){
+        super(context);
+    }
+
+    public void onDraw (Canvas canvas){
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        if(droid == null){
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droid);
+            droid = new Droid(bitmap, 0, 0, this);
+        }
+
+        if(ground == null){
+            ground = new Ground(0, height - START_GROUND_HEIGHT, width, height);
+
+        }
+        droid.move();
+        droid.draw(canvas);
+        ground.draw(canvas);
+
+        invalidate();
+    }
+    @Override
+    public int getDistanceFromGround(Droid droid){
+        return ground.rect.top - droid.rect.bottom;
+
+    }
+
+}
---
+++ kyu_WeatherForecasts/app/src/main/java/jp/co/july/tokyo/weatherforecasts/MainActivity.java
@@ -0,0 +1,60 @@
+package jp.co.july.tokyo.weatherforecasts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tv_main);
+        
+        try{
+            String data = WeatherApi.getWeather(this, "400040");
+            textView.setText(data);
+        }catch (IOException e) {
+            Toast.makeText(this, "IOException is occured", Tost.LENGTH_SHORT).show();
+            
+        }
+        
+    }
+    
+   
+    
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
